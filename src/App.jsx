906c5c3098bba3c4e23bf52cf59e8ff47e4b09e3
Rich-1007 <ghricheak@gmail.com>
2024-03-03 22:46:02 +0530

    Bulding a E-commerce site, using Redux Toolkit.

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <div>
+        <div>
+          <Navbar />
+        </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="cart" element={<Cart />} />
+        </Routes>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import { FaCartShopping } from "react-icons/fa6";
+
+export default function Navbar() {
+  return (
+    <div>
+      <div className="flex flex-row justify-between">
+        <img src="" />
+        <p>Home</p>
+        <FaCartShopping />
+      </div>
+    </div>
+  );
+}
