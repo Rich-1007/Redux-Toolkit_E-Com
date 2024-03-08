467e26d89822e66f8e14e61ebeaaf70883deae96
Rich-1007 <ghricheak@gmail.com>
2024-03-08 23:45:09 +0530

    upDates

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -3,6 +3,14 @@ import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import CartItem from "../components/CartItem";
 
+
+
+const Cart = () => {
+  const { cart } = useSelector((state) => state);
+
+  const [totalAmount, setTotalAmount] = useState(0);
+
+
   useEffect(() => {
     setTotalAmount(cart.reduce((acc, curr) => acc + curr.price, 0));
   }, [cart]);
