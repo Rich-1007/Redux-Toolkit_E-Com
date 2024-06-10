3ef4bea70562d822225f3bb7b1c3afb401753cf5
Rich-1007 <ghricheak@gmail.com>
2024-06-10 23:01:55 +0530

    New Code Updates

--- src/components12/CartItem.jsx
+++ src/components12/CartItem.jsx
@@ -1,49 +1,49 @@
-import { MdOutlineDeleteForever } from "react-icons/md";
-import { useDispatch } from "react-redux";
+// import { MdOutlineDeleteForever } from "react-icons/md";
+// import { useDispatch } from "react-redux";
 
-import { remove } from "../Redux/slices/CartSlice";
-import { toast } from "react-hot-toast";
+// import { remove } from "../Redux/slices/CartSlice";
+// import { toast } from "react-hot-toast";
 
-const CartItem = ({ item, itemIndex }) => {
-  const dispatch = useDispatch();
+// const CartItem = ({ item, itemIndex }) => {
+//   const dispatch = useDispatch();
 
-  const removeFromCart = () => {
-    dispatch(remove(item.id));
-    toast.success("Item Removed");
-  };
+//   const removeFromCart = () => {
+//     dispatch(remove(item.id));
+//     toast.success("Item Removed");
+//   };
 
-  return (
-    <div className="flex justify-center">
-      <div
-        className="flex flex-col items-center justify-center 
-      transition-shadow hover:shadow-2xl hover:shadow-black/100 ease-in gap-3 p-4 mt-10 ml-5 rounded-xl"
-      >
-        <div className="flex gap-4">
-          <div className="h-[180px] ">
-            <img className="h-full w-full" src={item.image} />
-          </div>
+//   return (
+//     <div className="flex justify-center">
+//       <div
+//         className="flex flex-col items-center justify-center 
+//       transition-shadow hover:shadow-2xl hover:shadow-black/100 ease-in gap-3 p-4 mt-10 ml-5 rounded-xl"
+//       >
+//         <div className="flex gap-4">
+//           <div className="h-[180px] ">
+//             <img className="h-full w-full" src={item.image} />
+//           </div>
 
-          <div className="flex flex-col justify-between py-3">
-            <div className="space-y-1">
-              <h1 className="text-gray-700 font-semibold text-sm text-left truncate w-40 mt-1">
-                {item.title}
-              </h1>
-              <h1 className=" w-40 text-gray-400 font-normal text-[10px] text-left line-clamp-3">
-                {item.description}
-              </h1>
-            </div>
-            <hr />
-            <div className="flex justify-between gap-12 items-center w-full mt-5 ">
-              <p className="text-green-500 font-semibold">{item.price}</p>
-              <div onClick={removeFromCart} className="">
-                <MdOutlineDeleteForever className="cursor-pointer w-6 h-6 text-red-600" />
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
+//           <div className="flex flex-col justify-between py-3">
+//             <div className="space-y-1">
+//               <h1 className="text-gray-700 font-semibold text-sm text-left truncate w-40 mt-1">
+//                 {item.title}
+//               </h1>
+//               <h1 className=" w-40 text-gray-400 font-normal text-[10px] text-left line-clamp-3">
+//                 {item.description}
+//               </h1>
+//             </div>
+//             <hr />
+//             <div className="flex justify-between gap-12 items-center w-full mt-5 ">
+//               <p className="text-green-500 font-semibold">{item.price}</p>
+//               <div onClick={removeFromCart} className="">
+//                 <MdOutlineDeleteForever className="cursor-pointer w-6 h-6 text-red-600" />
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
 
-export default CartItem;
+// export default CartItem;
--- src/components12/Product.jsx
+++ src/components12/Product.jsx
@@ -1,69 +1,69 @@
-import { toast } from "react-hot-toast";
-import { useDispatch, useSelector } from "react-redux";
-import { add, remove } from "../Redux/slices/CartSlice";
+// import { toast } from "react-hot-toast";
+// import { useDispatch, useSelector } from "react-redux";
+// import { add, remove } from "../Redux/slices/CartSlice";
 
-const Product = ({ post }) => {
+// const Product = ({ post }) => {
 
   
-  const { cart } = useSelector((store) => store);
-  const dispatch = useDispatch();
+//   const { cart } = useSelector((store) => store);
+//   const dispatch = useDispatch();
 
-  const addToCart = () => {
-    dispatch(add(post));
-    toast.success("Item added to Cart");
-  };
+//   const addToCart = () => {
+//     dispatch(add(post));
+//     toast.success("Item added to Cart");
+//   };
 
-  const removeFromCart = () => {
-    dispatch(remove(post.id));
-    toast.error("Item removed from Cart");
-  };
+//   const removeFromCart = () => {
+//     dispatch(remove(post.id));
+//     toast.error("Item removed from Cart");
+//   };
 
-  return (
-    <div
-      className="  flex flex-col items-center justify-center 
-    transition-shadow hover:shadow-2xl hover:shadow-black/100 ease-in gap-3 p-4 mt-10 ml-5 rounded-xl"
-    >
-      <div>
-        <p className="text-gray-700 font-semibold text-sm text-left truncate w-40 mt-1">
-          {post.title}
-        </p>
-      </div>
+//   return (
+//     <div
+//       className="  flex flex-col items-center justify-center 
+//     transition-shadow hover:shadow-2xl hover:shadow-black/100 ease-in gap-3 p-4 mt-10 ml-5 rounded-xl"
+//     >
+//       <div>
+//         <p className="text-gray-700 font-semibold text-sm text-left truncate w-40 mt-1">
+//           {post.title}
+//         </p>
+//       </div>
 
-      <div className="line-clamp-3">
-        <p className=" w-40 text-gray-400 font-normal text-[10px] text-left">
-          {post.description}
-        </p>
-      </div>
+//       <div className="line-clamp-3">
+//         <p className=" w-40 text-gray-400 font-normal text-[10px] text-left">
+//           {post.description}
+//         </p>
+//       </div>
 
-      <div className="h-[180px] ">
-        <img src={post.image} className="h-full w-full" />
-      </div>
+//       <div className="h-[180px] ">
+//         <img src={post.image} className="h-full w-full" />
+//       </div>
 
-      <div className="flex justify-between gap-12 items-center w-full mt-5">
-        <div>
-          <p className="text-green-500 font-semibold">{post.price} $</p>
-        </div>
+//       <div className="flex justify-between gap-12 items-center w-full mt-5">
+//         <div>
+//           <p className="text-green-500 font-semibold">{post.price} $</p>
+//         </div>
 
-        {cart.some((p) => p.id == post.id) ? (
-          <button
-            className="text-gray-700 border-2 border-gray-700 rounded-full font-semibold text[12px] p-1 px-2  uppercase 
-        hover:bg-gray-600 hover:text-white transition duration-300 ease-in "
-            onClick={removeFromCart}
-          >
-            Remove Item
-          </button>
-        ) : (
-          <button
-            className="text-gray-700 border-2 border-gray-700 rounded-full font-semibold text[12px] p-1 px-2 uppercase 
-        hover:bg-gray-600 hover:text-white transition duration-300 ease-in "
-            onClick={addToCart}
-          >
-            Add To Cart
-          </button>
-        )}
-      </div>
-    </div>
-  );
-};
+//         {cart.some((p) => p.id == post.id) ? (
+//           <button
+//             className="text-gray-700 border-2 border-gray-700 rounded-full font-semibold text[12px] p-1 px-2  uppercase 
+//         hover:bg-gray-600 hover:text-white transition duration-300 ease-in "
+//             onClick={removeFromCart}
+//           >
+//             Remove Item
+//           </button>
+//         ) : (
+//           <button
+//             className="text-gray-700 border-2 border-gray-700 rounded-full font-semibold text[12px] p-1 px-2 uppercase 
+//         hover:bg-gray-600 hover:text-white transition duration-300 ease-in "
+//             onClick={addToCart}
+//           >
+//             Add To Cart
+//           </button>
+//         )}
+//       </div>
+//     </div>
+//   );
+// };
 
-export default Product;
+// export default Product;
